4e21c6292334cd1de2076ee8c171667e7e938f7a
Arya Tandon <atandon23@outlook.com>
2021-02-17 10:10:57 +0000

    Exercide 1 & first part of 2 completed

---
+++ shopping-list-app/src/App.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './App.css';
+
+let mappedList: any;
+
+function App() {
+
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [list, setList] = useState([['']]);
+  const [unit, setUnit] = useState("kilo");
+
+  function handleChange( event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> ) {
+    console.log(event);
+    const {name, value} = event.target
+    console.log({name, value});
+    name === "Item" ? setItem(value)
+    : name === "Quantity" ? setQuantity(value)
+    : setUnit(value)
+  }
+
+  function addItem () {
+    const addInfo = [item, quantity, unit];
+    setList([...list, addInfo]);
+    console.log(list)
+  }
+
+  function deleteItem (index: number) {
+    let list2 = list;
+    list2.splice(index, 1);
+    setList([...list2]);
+  }
+
+  function showList ( [ item, quantity, unit ]: string[], index: number ) {
+    return (
+    <div>
+      <p>{item}</p>
+      <p>{quantity}</p>
+      <p>{unit}</p>
+      <button onClick = {() => deleteItem(index)}> Delete item </button>
+      <hr />
+    </div>
+    )
+  }
+
+  mappedList = list.map(showList)
+
+  return (
+    <div>
+      <form>
+        <label>
+          Item:
+          <input
+            name="Item"
+            type="string"
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Quantity:
+          <input
+            name="Quantity"
+            type="number"
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Pick a unit:
+            <select name="Dropdown" onChange={handleChange}>
+                <option value="kilo" selected>Kilograms</option>
+                <option value="litres" >Litres</option>
+            </select>
+        </label>
+      </form>
+      <button onClick = {() => addItem()}> Add item </button>
+      {mappedList}
+    </div>
+  )
+}
+
+// function renderApp() {
+//   ReactDOM.render(<App />, document.getElementById('root'))
+// }
+
+// renderApp()
+
+export default App;
